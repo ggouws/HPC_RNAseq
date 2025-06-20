d0f6565b647fc5aab050e44117953653e3092754
ggouws <136603000+ggouws@users.noreply.github.com>
2025-06-20 17:39:31 +0100

    Add files via upload

--- scripts/07_htseq-count.sh
+++ scripts/07_htseq-count.sh
@@ -12,33 +12,38 @@
 #SBATCH --time=72:00:00
 
 source ~/.bash_profile
-#####GET THE HTSEQ ENVIRONMENT
-conda activate hisat2
+conda activate htseq
 
 helpFunction()
 {
   echo ""
-  echo "Usage: $0 -m parameterM -t parameterT -i parameterI -r parameterR"
+  echo "Usage: $0 -m parameterM -s parameterS -t parameterT -i parameterI -r parameterR -A parameterA -X parameterX"
   echo -e "\t-m mode for handling reads overlapping more than one genomic feature"
+  echo -e "\t-s indication of whether the data are stranded ('yes') or not ('no')
   echo -e "\t-t the feature to be used (e.g., gene or exon)"
   echo -e "\t-i GFF attribute used to identify the feature"
-  echo -e "\t-r alignment sorted by read name ('name') or alignment position ('pos')
+  echo -e "\t-r alignment sorted by read name ('name') or alignment position ('pos')"
+  echo -e "\t-A start of the name or accession number of genomic/transcriptomic annotation file"
+  echo -e "\t-X extension of the the genomic/transcriptomic annotation file"
   exit 1 # Exit script after printing help
 }
 
-while getopts "m:t:i:r:" opt
+while getopts "m:s:t:i:r:A:X:" opt
 do
    case "$opt" in
-      m ) parameterA="$OPTARG" ;;
+      m ) parameterM="$OPTARG" ;;
+      s ) parameterS="$OPTARG" ;;
       t ) parameterT="$OPTARG" ;;
       i ) parameterI="$OPTARG" ;;
       r ) parameterR="$OPTARG" ;;
+      A ) parameterA="$OPTARG" ;;
+      X ) parameterX="$OPTARG" ;;
       ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
    esac
 done
 
 # Print helpFunction in case parameters are empty
-if [ -z "$parameterA" ] || [ -z "$parameterT" ] || [ -z "$parameterI" ] || [ -z "$parameterR" ]
+if [ -z "$parameterM" ] || [ -z "$parameterS" ] || [ -z "$parameterT" ] || [ -z "$parameterI" ] || [ -z "$parameterR" ] || [ -z "$parameterA" ] || [ -z "$parameterX" ]
 then
    echo "One or more of the mandatory arguments is/are missing. Please check these your command line submission.";
    helpFunction
@@ -55,13 +60,12 @@ do
 FBASE=$(basename $f)
 BASE=${FBASE%.clean.bam};
 htseq-count \
--m parameterM \
--s yes \
--t parameterT \
--i parameterI \
--r parameterR \
+-m "$parameterM" \
+-s "$parameterS" \
+-t "$parameterT" \
+-i "$parameterI" \
+-r "$parameterR" \
 -f bam \
-$src/aligned_clean/${BASE}.clean.bam \
-$src/reference/*GFF_FILE_HERE > \
-$src/htseq/${BASE}_genecount.htseq;
+"$src/aligned_clean/${BASE}.clean.bam" "$src/reference/"$parameterA"*"$parameterX" > \
+"$src/htseq/${BASE}_genecount.htseq";
 done
